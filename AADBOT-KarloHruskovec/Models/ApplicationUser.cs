2a6acecf38c29f78ff676664036ac3b7dc8dca87
Karlo Hruskovec <hruskovec.karlo@gmail.com>
2025-06-15 18:46:46 +0200

    Add project files.

---
+++ AADBOT-KarloHruskovec/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AADBOT_KarloHruskovec.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+		public string Package { get; set; } = "FREE"; // FREE, PRO, GOLD
+		public DateTime? LastPackageChange { get; set; }
+		public long DailyUploadSize { get; set; } = 0; // in bytes
+		public DateTime? LastUploadReset { get; set; }
+	}
+}
---
+++ AADBOT-KarloHruskovec/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AADBOT_KarloHruskovec.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
